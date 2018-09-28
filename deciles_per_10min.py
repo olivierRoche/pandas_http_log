0b2bea0ce849c98e303b37ad3b16330b1629510b
Olivier Roche <olivier.a.roche@gmail.com>
2018-09-28 11:30:06 +0200

    made the file usable, readable (in english)

--- deciles_per_10min.py
+++ deciles_per_10min.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
 
-# In[ ]:
-
 
 import pandas as pd
 import numpy as np
@@ -10,130 +8,69 @@ import matplotlib.pyplot as plt
 import pytz
 
 
-# In[ ]:
-
-
-data=pd.read_csv("access-beton_sf_prod-api.log", sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])', 
-                 engine='python', na_values=['-'],header=None,usecols=[0, 3, 4, 5, 6, 7, 8,10],names=
-                 ['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent','req_time'], 
-                 converters={'status': int, 'size': int,'req_time': int})
-data
-
-
-# In[ ]:
-
-
-def read_hour(date):
-    hour=float(date[13:15])
-    minutes=float(date[16:18])
-    return hour+minutes/100
-
-
-# In[ ]:
-
-
-def segment_time(date,tic):
-    #renvoie le début de la tranche de tic minutes correspondant à l'heure date, 
-    # par exemple, avec tic = 10, et date=[19/Sep/2018:14:59:03 +0200], le résultat, qui correspond à 14h50, sera 14.5
-    # les segments sont comptés à partir de 00h00, ainsi avec un tic=25, le résultat pour l'heure 01h00 sera 0.5 (ie 0h50)
-    hour=int(date[13:15])
-    minutes=int(date[16:18])
-    time_in_minutes=60*hour+minutes
-    tic_start=time_in_minutes - time_in_minutes % tic    
-    return tic_start // 60 + float(tic_start % 60)/100
-
-
-# In[ ]:
-
-
-data['req_time']=data['req_time'].apply(lambda x: x / 1000)
-
-
-# In[ ]:
-
-
-data
-
-
-# In[ ]:
-
-
-def time_to_minutes(x):
-    return int(x[13:15])*60+int(x[16:18])
-
-def time_segment(x,tic):
-    return time_to_minutes(x)//tic
-
-data['tics']=data['time'].apply(lambda x : time_segment(x,10))
-data
-
-
-# In[ ]:
-
-
-deciles_by_tics=pd.DataFrame(columns=range(38,91))
-deciles_by_tics
-
-
-# In[ ]:
-
-
-def deciles(col):
-    return data[data['tics']==col]['req_time'].quantile([i/10 for i in range(10)])
-
-
-# In[ ]:
-
-
-for c in range(38,91):
-    deciles_by_tics[c]=deciles(c)
-    
-deciles_by_tics
-
-
-# In[ ]:
-
-
-#exprimons les déciles en fonction des tics et non l'inverse :
-deciles_by_tics=deciles_by_tics.T
-#puis remettons les tics sous un format lisible :
-deciles_by_tics.index=deciles_by_tics.index.map(lambda x:(x*10)//60 + (10*x%60)/100)
-
-
-# In[ ]:
-
-
-deciles_by_tics.plot(kind='bar',legend=True)
-
-
-# In[ ]:
-
-
-#à part pendant le pic évoqué, 80% des requêtes durent moins de 0.5s
-
-
-# In[ ]:
-
-
-# lorsqu'on empile les barres, ça ne va pas car les centiles se cumulent :
-deciles_by_tics.plot(kind='bar',stacked=True)
-
-
-# In[ ]:
-
-
-#ça ne va pas car les déciles sont additionnés au lieu de se superposer...
-
-
-# In[ ]:
-
-
-#finallement, je trouve le plot de base plus lisible :
-deciles_by_tics.plot()
-
-
-# In[ ]:
-
-
-deciles_by_tics
-
+'''
+- tout en anglais
+- clean du style pep8
+- supprimer tout ce qui est inutile
+- fonction pour tout
+- fonction un peu plus globale pour les utiliser dans jupyter
+- mettre des exemples dans les fonctions
+le code dez jupyter ca va devenir
+ import deciles_per_10min
+ load_fie()
+ process()
+ show_graph_decile()
+ 
+'''
+
+def load_log(log_name):
+    data=pd.read_csv(log_name,
+                 sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])', 
+                 engine='python', na_values=['-'], header=None,
+                 usecols=[0, 3, 4, 5, 6, 7, 8,10],
+                 names=['ip', 'time', 'request', 'status', 'size', 'referer',
+                 'user_agent','req_time'], 
+                 converters={'status': int, 'size': int, 'req_time': int})
+    #it is more convenient to have the request time in millisecond rather than
+    #microsecond :
+    data['req_time'] = data['req_time'].apply(lambda x: x / 1000) 
+    return data
+
+
+def time_to_minutes(date):
+    #returns the time given by date, as a number of minutes ellapsed since 0:00
+    #eg with date = "[19/Sep/2018:06:25:05 +0200]", it is 6:25 and the
+    #function returns 6*60+25=385.
+    return int(date[13:15])*60+int(date[16:18])
+
+'''
+      *****TERMINOLOGY*****
+We want to divide the time of the day into segments.
+We call __duration__ the length of the segments (in minutes).
+We call __tic__ the index of a segment. 
+
+For example, say duration = 10 minutes, the 8th segment is [70,80]. Its tic
+is 7. It is convenient to refer to this segment as the tic 7.
+Hence, the tic n refers to the time segment [(n-1)*duration, n*duration].
+'''
+
+def add_tics(data,duration):
+    data['tics']=data['time'].apply(lambda x : time_to_minutes(x)//duration)
+
+
+def deciles(data,tic):
+    return data[data['tics']==tic]['req_time'].quantile([i/10 
+                                                         for i in range(10)])
+
+def get_deciles_by_tic(data):
+    first_tic=data['tics'].min()
+    last_tic=data['tics'].max()
+    deciles_by_tics=pd.DataFrame(columns=range(first_tic,last_tic+1))
+    for c in range(first_tic,last_tic+1):
+        deciles_by_tics[c]=deciles(data,c)
+    return deciles_by_tics.T
+
+def readable_time(tic,duration):
+    #returns the time at which the tic begins. If, eg, the tic begins at 5:25,
+    #this will return the float 5.25.
+    return duration*tic//60 + (duration*tic%60)/100
